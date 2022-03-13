c079f978cdaeaa740f163dfbe1261c20565ae137
Islamiyat-22 <isibor.islamiyat@gmail.com>
2022-03-13 16:59:30 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  * main - prints the phrase with proper grammar,
@@ -15,9 +13,7 @@
 int main(void)
 
 {
-
 		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+		return (0);
 
 }
