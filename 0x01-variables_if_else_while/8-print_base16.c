621267eb1269538be4fd96554d433cd93c7149c8
Islamiyat-22 <isibor.islamiyat@gmail.com>
2022-03-14 15:29:43 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,16 +5,15 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
-	
-	char ch;
+		char ch;
 
 	for (n = 48; n < 58; n++)
 
 	{
-		
 		putchar(n);
 		
 		for (ch = 'a'; ch <= 'f'; ch++)
