c1b28ef40cc8ae7b39d546def7ad6544877d2c2d
Islamiyat-22 <isibor.islamiyat@gmail.com>
2022-03-16 11:43:34 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,26 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet
+ * main - check the code
  *
- * Return: void
+ * Return: Always 0.
  */
 
 void print_alphabet(void)
-
 {
-	
 	char ch = 'a';
-	
+
 	while (ch <= 'z')
 	{
-		
 		_putchar(ch);
-		
-		ch++;
-	
-	}
-	
-	_putchar('\n');
 
+		ch++;
+	}
+	_putchar('\n');
 }
