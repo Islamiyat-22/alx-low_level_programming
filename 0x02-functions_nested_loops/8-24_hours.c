7fff381d282acec054ad607ca51b2592343ccd62
Islamiyat-22 <isibor.islamiyat@gmail.com>
2022-03-17 04:14:46 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,6 @@ void jack_bauer(void)
 
 {
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
-	
 	hours_max = 58;
 	hours_tens = '0';
 	while (hours_tens < '3')
@@ -37,7 +36,6 @@ void jack_bauer(void)
 					_putchar(minutes_ones);
 					_putchar('\n');
 					minutes_ones++;
-				
 				}
 				minutes_ones = '0';
 				minutes_tens++;
