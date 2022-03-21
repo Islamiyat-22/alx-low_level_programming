88a3c1a4c5318e4f250c7d6dd5a0bd92fa909eb1
Islamiyat-22 <isibor.islamiyat@gmail.com>
2022-03-21 16:00:34 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,7 @@
 void print_rev(char *s)
 {
 	int i = 0;
-
+	
 	while (s[i] != '\0')
 	{
 		i++;
@@ -18,7 +18,7 @@ void print_rev(char *s)
 	while (i >= 0)
 	{
 		_putchar(s[i]);
-		i--
+		i--;
 	}
 	_putchar('\n');
 }
