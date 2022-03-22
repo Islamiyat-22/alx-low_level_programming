e427e383caa617ac028b5c3473ed21776fedd25f
Islamiyat-22 <isibor.islamiyat@gmail.com>
2022-03-21 17:05:07 -0700

    tasks

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,15 +7,13 @@
  * Return: char with copy of string
  */
 
-char *_stecpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i =m 0;
 
-	for (i = 0; *(src + i) != '\0'; i++)
+	for (; src[i] != '\0'; i++)
 	{
-		dest[i] = *(src + i);
+		dest[i] = src[i];
 	}
-	dest[i] = '\0';
-
 	return (dest);
 }
