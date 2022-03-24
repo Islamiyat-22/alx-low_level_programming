741b808bbd9ad5ba065ffe834d78a1899197ccd3
Islamiyat-22 <isibor.islamiyat@gmail.com>
2022-03-23 18:55:36 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,30 @@
 #include "main.h"
 
 /**
- * leet - Entry point
- * ONE if, TWO loops only...
- * @n: input
- * Return: Always 0 (Success)
+ * leet - encodes a string into 1337
+ * @s: input string
+ * Return: the pointer to dest.
  */
 
-char *leet(char *n)
+char *leet(char *s)
 {
-	int i, x;
-	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T','l', 'L'};
-	int replacer[] = {'4', '3', '0', '7', '1'};
-
-	for (i = 0; n[i] != '\0'; i++)
+	int count = o, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
+	int replacer[] = {52, 51, 48, 55, 49};
+	
+	while (*(s + count) != '\0')
 	{
-		for (x = 0; x <= 9; x++)
+		for (i = 0; i < 5; i++)
 		{
-			if (n[i] == find[x])
-			{
-				n[i] = replacwe[x / 2];
-				x = 9;
-			}
+			if (*(s + count == low_letters[i] || *(s + count) == upp_letters[i])
+					{
+						*(s + count) = replacer[i];
+						break;
+					}
 		}
+		count++;
 	}
 
-	return (n);
+	return (s);
 }
